68a80b60c620e6ba55e684040b2d60930d638f6b
parabojas13-maker <parabojas13@gmail.com>
2026-02-08 22:42:19 +0530

    Add list operations in Question_1.py

---
+++ python 2/unit_1/Question_1.py
@@ -0,0 +1,16 @@
+my_list = [40, 10, 30, 20]
+
+print(my_list[0])
+print(my_list[-1])
+
+my_list.append(50)
+my_list.insert(1, 15)
+
+my_list.remove(10)
+my_list.pop()
+
+my_list.sort()
+print(my_list)
+
+my_list.reverse()
+print(my_list)
